19a28d54e763af48b5e5b1e18fe2e27c2e5f789e
Pranjal Walia <pranjalwalia77@gmail.com>
2023-02-04 17:15:09 +0530

    fix: fox cors

--- pages/admin/ifsc.js
+++ pages/admin/ifsc.js
@@ -14,33 +14,38 @@ import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 import Admin from "layouts/Admin.js";
 
 export default function IFSC() {
+  const [ifsc, setIfsc] = useState("");
 
-    const [ifsc, setIfsc] = useState("");
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(ifsc)
-        const res = await axios.post('https://63ae-119-161-98-68.in.ngrok.io/ifsc', {"ifscCode": "SBIN0002396"});
-        console.log(res)
-        console.log(res.data)
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(ifsc);
+    const res = await axios.post("http://localhost:4000/ifsc", {
+      ifscCode: ifsc,
+    });
+    console.log(res);
+    console.log(res.data);
+  };
 
   return (
     <>
-    <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3" onSubmit={handleSubmit}>
+      <form
+        className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
+        onSubmit={handleSubmit}
+      >
         <div className="relative flex w-full flex-wrap items-stretch">
-            <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+          <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
             <i className="fas fa-search"></i>
-            </span>
-            <input
-            onChange={(e) => {setIfsc(e.target.value)}}
+          </span>
+          <input
+            onChange={(e) => {
+              setIfsc(e.target.value);
+            }}
             type="text"
             placeholder="Enter IFSC Code..."
             className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
-            />
+          />
         </div>
-    </form>
-
+      </form>
     </>
   );
 }
